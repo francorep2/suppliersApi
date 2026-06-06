8e4255437c607cc4f35a6151e87df0fcf7d7cbd3
francorep2 <francorep2@gmail.com>
2026-06-05 22:30:59 -0300

    no env

--- src/main/java/com/suppliers_tgs_api/AplicationSuppliersTgsApi.java
+++ src/main/java/com/suppliers_tgs_api/AplicationSuppliersTgsApi.java
@@ -3,7 +3,6 @@ package com.suppliers_tgs_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import com.suppliers_tgs_api.loader.EnvConfig;
 
 
 @SpringBootApplication
@@ -12,7 +11,6 @@ public class AplicationSuppliersTgsApi {
 
 public static void main(String[] args) {
     try {
-        EnvConfig.load();
         System.out.println("ENV loaded");
         
         SpringApplication.run(AplicationSuppliersTgsApi.class, args);
